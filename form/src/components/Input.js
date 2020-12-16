f985d71c86b3ea67275d38751cde225dd9c160be
Aszalea Calderon <aszalea_calderon@yahoo.com>
2020-12-16 13:59:51 -0800

    more things

--- form/src/components/Input.js
+++ form/src/components/Input.js
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Input = () => {
+  const [form, setForm] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    passwordConfirm: "",
+    terms: false,
+  });
+  const change = (e) => {
+    const { value, type, checked, name } = e.target;
+    const valueToUse = type === "checkbox" ? checked : value;
+    setForm({ ...form, [name]: valueToUse });
+  };
+
   return (
     <>
       <form>
         <div className="sideBySide">
           <input
+            onChange={change}
+            value={form.firstName}
+            name="firstName"
             className="small"
             type="text"
             placeholder="  First Name"
           ></input>
           <br />
           <input
+            onChange={change}
+            value={form.lastName}
+            name="lastName"
             className="small"
             type="text"
             placeholder="  Last Name"
@@ -19,18 +39,43 @@ const Input = () => {
         </div>
         <br />
         <div className="form">
-          <input className="large" type="text" placeholder="  Email"></input>
-          <br />
-          <input className="large" type="text" placeholder="  Password"></input>
+          <input
+            onChange={change}
+            value={form.email}
+            name="email"
+            className="large"
+            type="text"
+            placeholder="  Email"
+          ></input>
           <br />
           <input
+            onChange={change}
+            value={form.password}
+            name="password"
+            className="large"
+            type="text"
+            placeholder="  Password"
+          ></input>
+          <br />
+          <input
+            onChange={change}
+            value={form.passwordConfirm}
+            name="passwordConfirm"
             className="large"
             type="text"
             placeholder="  Confirm Password"
           ></input>
           <br />
-          <input className="checkbox" type="checkbox"></input>
-          <p>Agree to Terms and Conditions</p>
+          <label>
+            <input
+              onChange={change}
+              value={form.checkbox}
+              name="terms"
+              className="checkbox"
+              type="checkbox"
+            ></input>
+            Agree to Terms and Conditions
+          </label>
         </div>
       </form>
     </>
